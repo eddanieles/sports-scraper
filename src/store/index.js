b17fb14ea9e679c10f0ad6e11cb394f8a17fa5bc
Ed Danieles <eddanieles@gmail.com>
2021-06-03 14:51:55 -0400

    persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,46 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import request from 'request'
+import cheerio from 'cheerio'
 
 Vue.use(Vuex)
 
+const corsBridge = 'https://cors.bridged.cc/'
+const nflMvpsUrl = 'https://www.pro-football-reference.com/awards/ap-nfl-mvp-award.htm'
+
 export default new Vuex.Store({
   state: {
+    nflMvps: []
   },
+  plugins: [createPersistedState()],
   mutations: {
+    setNflMvps(state, val) {
+      state.nflMvps = val
+    }
   },
   actions: {
+    getNflMvps({commit}) {
+      let mvpArr = [];
+      request(`${corsBridge}${nflMvpsUrl}`, (error, response, html) => {
+          if(!error && response.statusCode === 200) {
+              const $ = cheerio.load(html);
+
+              let column = $('#awards > tbody > tr');
+
+              for (let i=0; i < column.length; i++) {
+                mvpArr.push({
+                  year: column[i].children[0].children[0].children[0].data,
+                  position: column[i].children[2].children[0].data,
+                  player: column[i].children[3].children[0].children[0].data,
+                  team: column[i].children[4].children[0].children[0].data
+                })
+              }
+
+              commit('setNflMvps', mvpArr);
+          }
+      })
+    }
   },
   modules: {
   }
--- src/views/Football.vue
+++ src/views/Football.vue
@@ -1,71 +1,57 @@
 <template>
   <div class="about">
-    <!-- <p>{{this.mvpArr}}</p> -->
     <div v-for="team in this.teams" :key="team.teamName">
       <p>{{team.teamName}}</p>
       <h3>Count: {{team.count}}</h3>
-      <p>{{team.players}}</p>
+      <ul>
+        <li v-for="player in team.players" :key="player.year">
+          {{player.year}}: {{player.player}} - {{player.position}}
+        </li>
+      </ul>
       <hr>
     </div>
   </div>
 </template>
 
 <script>
-import request from 'request'
-import cheerio from 'cheerio'
-import _ from 'lodash'
+import _ from 'underscore'
 
 export default {
   data() {
     return {
-      mvpArr: [],
       teams: []
     }
   },
   beforeCreate() {
+    this.$store.dispatch('getNflMvps');
+  },
+  created() {
     let that = this;
-    request('https://cors.bridged.cc/https://www.pro-football-reference.com/awards/ap-nfl-mvp-award.htm', (error, response, html) => {
-        if(!error && response.statusCode === 200) {
-            const $ = cheerio.load(html);
-
-            let column = $('#awards > tbody > tr');
-
-            for (let i=0; i < column.length; i++) {
-              that.mvpArr.push({
-                year: column[i].children[0].children[0].children[0].data,
-                player: column[i].children[3].children[0].children[0].data,
-                team: column[i].children[4].children[0].children[0].data
-              })
-            }
-
-            let justTeamNames = []
-            that.mvpArr.forEach(obj => {
-              if (_.indexOf(justTeamNames, obj.team) === -1) {
-                justTeamNames.push(obj.team);
-                that.teams.push({
-                  teamName: obj.team, 
-                  count: 1, 
-                  players: [{year: obj.year, player: obj.player}]
-                });
-              } else {
-                // count: count++,
-                // players: players.push({year: obj.year, player: obj.player})
-                let index = _.findIndex(that.teams, function(o) {
-                  return o.teamName === obj.team
-                })
-                // console.log("index", index)
-                that.teams[index].count++;
-                that.teams[index].players.push({year: obj.year, player: obj.player})
-              }
-            })
-
-            that.teams = _.sortBy(that.teams, ['count', 'year']).reverse()
-        }
+    let justTeamNames = []
+    this.$store.state.nflMvps.forEach(obj => {
+      if (_.indexOf(justTeamNames, obj.team) === -1) {
+        justTeamNames.push(obj.team);
+        that.teams.push({
+          teamName: obj.team, 
+          count: 1, 
+          players: [{year: obj.year, player: obj.player, position: obj.position}]
+        });
+      } else {
+        let index = _.findIndex(that.teams, function(o) {
+          return o.teamName === obj.team
+        })
+        that.teams[index].count++;
+        that.teams[index].players.push({year: obj.year, player: obj.player, position: obj.position})
+      }
     })
+
+    that.teams = _.sortBy(that.teams, ['count']).reverse();
   }
 }
 </script>
 
 <style>
-
+li {
+  list-style-type: none;
+}
 </style>
