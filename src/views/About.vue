ec4971c5fc3de8138c4db9dd4465a82e4ca7c0d2
Ed Danieles <eddanieles@gmail.com>
2021-06-03 17:34:30 -0400

    by player

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-tabs 
+      active-nav-item-class="font-weight-bold text-uppercase text-danger"
+      active-tab-class="font-weight-bold text-success"
+      content-class="mt-3"
+      vertical>
+      <b-tab title="First" active><p>I'm the first tab</p></b-tab>
+      <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+      <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+    </b-tabs>
   </div>
 </template>
--- src/views/Football.vue
+++ src/views/Football.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="about">
-    <div v-for="team in this.teams" :key="team.teamName">
+    <div>
+      <ul>
+        <li>As List</li>
+        <li @click="getByTeam()">By Team</li>
+        <li @click="getByPlayer()">By Player</li>
+        <li @click="getByPosition()">By Position</li>
+      </ul>
+    </div>
+    <div v-for="player in this.players" :key="player.player">
+      <p>{{player.player}} | {{player.position}}</p>
+      <h3>Count: {{player.count}}</h3>
+      <p>{{player.years}}</p>
+      <hr>
+    </div>
+    <!-- <div v-for="team in this.teams" :key="team.teamName">
       <p>{{team.teamName}}</p>
       <h3>Count: {{team.count}}</h3>
       <ul>
@@ -9,7 +23,7 @@
         </li>
       </ul>
       <hr>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -19,33 +33,63 @@ import _ from 'underscore'
 export default {
   data() {
     return {
-      teams: []
+      teams: [],
+      players: []
+    }
+  },
+  methods: {
+    getByTeam() {
+      let that = this;
+      let justTeamNames = []
+      this.$store.state.nflMvps.forEach(obj => {
+        if (_.indexOf(justTeamNames, obj.team) === -1) {
+          justTeamNames.push(obj.team);
+          that.teams.push({
+            teamName: obj.team, 
+            count: 1, 
+            players: [{year: obj.year, player: obj.player, position: obj.position}]
+          });
+        } else {
+          let index = _.findIndex(that.teams, function(o) {
+            return o.teamName === obj.team
+          })
+          that.teams[index].count++;
+          that.teams[index].players.push({year: obj.year, player: obj.player, position: obj.position})
+        }
+      })
+
+      that.teams = _.sortBy(that.teams, ['count']).reverse();
+    },
+    getByPlayer() {
+      let that = this;
+      let justPlayerNames = []
+      this.$store.state.nflMvps.forEach(obj => {
+        if (_.indexOf(justPlayerNames, obj.player) === -1) {
+          justPlayerNames.push(obj.player);
+          that.players.push({
+            player: obj.player,
+            position: obj.position,
+            count: 1, 
+            years: [{year: obj.year, team: obj.team}]
+          });
+        } 
+        else {
+          let index = _.findIndex(that.players, function(o) {
+            return o.player === obj.player
+          })
+          that.players[index].count++;
+          that.players[index].years.push({year: obj.year, team: obj.team})
+        }
+      })
+
+      that.players = _.sortBy(that.players, ['count']).reverse();
     }
   },
   beforeCreate() {
     this.$store.dispatch('getNflMvps');
   },
   created() {
-    let that = this;
-    let justTeamNames = []
-    this.$store.state.nflMvps.forEach(obj => {
-      if (_.indexOf(justTeamNames, obj.team) === -1) {
-        justTeamNames.push(obj.team);
-        that.teams.push({
-          teamName: obj.team, 
-          count: 1, 
-          players: [{year: obj.year, player: obj.player, position: obj.position}]
-        });
-      } else {
-        let index = _.findIndex(that.teams, function(o) {
-          return o.teamName === obj.team
-        })
-        that.teams[index].count++;
-        that.teams[index].players.push({year: obj.year, player: obj.player, position: obj.position})
-      }
-    })
-
-    that.teams = _.sortBy(that.teams, ['count']).reverse();
+    
   }
 }
 </script>
