97d77ac17233aed53a2e5305eaf5447ecfde25d1
Ed Danieles <eddanieles@gmail.com>
2021-06-28 20:13:03 -0400

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,31 +6,17 @@
       active-tab-class="font-weight-bold text-success"
       content-class="mt-3"
       vertical>
-      <b-tab title="First" active><p>I'm the first tab</p></b-tab>
-      <b-tab title="Second"><p>I'm the second tab</p></b-tab>
+      <b-tab title="First" active><p class="animate-spin">I'm the first tab</p></b-tab>
+      <b-tab title="Second">
+        <div class="border-black border-8"></div>
+      </b-tab>
       <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
     </b-tabs>
 
-    <v-menu offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
-          Dropdown
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index"
-        >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <!-- <div class="container" :style="{ background: gradient }">
+      <div class="circle" :style="{ background: gradient2 }"></div>
+    </div> -->
+  
   </div>
 </template>
 
@@ -44,6 +30,42 @@
         { title: 'Click Me 2' },
       ],
       offset: true,
+      xPosition: '',
+      yPosition: ''
     }),
+    computed: {
+      gradient() {
+        return `linear-gradient(to right, #61b${this.xPosition}, #a34${this.yPosition})`
+      },
+      gradient2() {
+        return `linear-gradient(to right, #a34${this.yPosition}, #61b${this.xPosition})`
+      }
+    },
+    methods: {
+      mousemove(event) {
+        this.xPosition = event.clientX;
+        this.yPosition = event.clientY;
+      }
+    },
+    mounted() {
+      window.addEventListener('mousemove', this.mousemove)
+    }
   }
 </script>
+
+<style scoped>
+.container {
+  width: 50em;
+  height: 50em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.circle {
+  width: 20em;
+  height: 20em;
+  /* background: linear-gradient(to right, #a345bb, #61b4d4); */
+  border-radius: 100%;
+}
+</style>
